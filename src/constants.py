50ac801ecef88ff6d8c8867d6aa0b1573c655961
shirin <75791599+shirin1996@users.noreply.github.com>
2022-01-19 21:25:40 +0330

    random connect bot+keyboards added

--- src/constants.py
+++ src/constants.py
@@ -3,11 +3,15 @@ from src.utils.keyboard import create_keyboard
 
 
 keys = SimpleNamespace(
-    settings='Setting :gear:',
     random_connect='Connect :handshake:',
-    help='Help :smiling_face_with_halo:')
+    help='Help :smiling_face_with_halo:',
+    exit=':cross_mark: Exit')
     
 keyboards = SimpleNamespace(
-    main_keyboard=create_keyboard(keys.random_connect, keys.settings,
-    keys.help)
+    exit=create_keyboard(keys.exit),
+    main_keyboard=create_keyboard(keys.random_connect, keys.help))
+
+states = SimpleNamespace(
+    random_connect='Your State is <strong>Random Connect!</strong>',
+    menu='You are in <strong>Menu!</strong>'
 )
--- src/run.py
+++ src/run.py
@@ -1,17 +1,17 @@
-import telebot
-import os
-from loguru import logger
-from src.constants import keyboards
-from src.utils.io import write_json
-from src.bot import bot
 import emoji
-from utils.filters import IsAdmin
-
+import pymongo
+from loguru import logger
 from telebot import custom_filters
+
+from src.bot import bot
+from src.constants import keyboards, keys, states
+from utils.filters import IsAdmin
 
 class Bot:
     def __init__(self, telebot):
         self.bot = telebot
+        client = pymongo.MongoClient("local host", 27017)
+        self.db = client.Msg_Bot
 
         #register handler
         self.handler()
@@ -26,18 +26,44 @@ class Bot:
 	
     def handler(self):
 
-        @self.bot.message_handler(func= lambda message:True)
-        def echo(message):
-            self.send_message(message.chat.id,message.text,
-            reply_markup=keyboards.main_keyboard)
+        @self.bot.message_handler(commands=['start'])
+        def start(message):
+            self.bot.send_message(message.chat.id,
+            f"Hiya <strong>{message.chat.first_name}</strong>, how can I help ya?")
+            user_info=message.json
+            user_info['state']=states.menu
+            self.db.users.update_one(
+                {'chat.id':message.chat.id},
+                {'$set':message.json}, upsert=True)
+
+        @self.bot.message_handler(regexp=emoji.emojize(keys.help))
+        def help(message):
+            self.send_message(
+                message.chat.id,
+                '<strong> PyBot:smiling_face_with_sunglasses:</strong> is to randomly connect ya to another person! \n Press `connect` to start!'
+                    ,reply_markup=keyboards.main_keyboard)
+
+        @self.bot.message_handler(regexp=emoji.emojize(keys.random_connect))
+        def random_connect(message):
+            self.send_message(
+                message.chat.id,'<strong> Connecting to :man_raising_hand: or :woman_raising_hand:</strong>'
+                    ,reply_markup=keyboards.exit)
 
         @self.bot.message_handler(is_admin=True)
         def admin_of_gp(message):
-            self.send_message(message.chat.id, '<strong> You are Admin!<strong>')
+            self.send_message(
+                message.chat.id, 
+                '<strong> You are Admin!</strong>')
 
         @self.bot.message_handler(text=['hi','hello'])
         def text_filter(message):
             self.send_message(message.chat.id, f'Hi {message.from_user.first_name}')
+
+        @self.bot.message_handler(func= lambda message:True)
+        def echo(message):
+            print(emoji.demojize(message.text))
+            self.send_message(message.chat.id,message.text,
+            reply_markup=keyboards.main_keyboard)
 
     def send_message(self, chat_id, text, reply_markup=None, emojize=True):
         if emojize: 
